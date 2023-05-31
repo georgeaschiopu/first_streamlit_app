8d77a0723449613b6b671d3ba00719cc2d63978f
georgeaschiopu <52062330+georgeaschiopu@users.noreply.github.com>
2023-05-31 16:55:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,9 @@ fruityvice_response=requests.get('https://fruityvice.com/api/fruit/'+fruit_choic
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
 
